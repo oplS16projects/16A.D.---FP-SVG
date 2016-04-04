0fcec402c0797df52f0402f88e6793a879d59ee4
DKheifets <dkheifets@gmail.com>
2016-04-04 01:35:29 -0400

    Added files via upload

---
+++ gui.rkt
@@ -0,0 +1,147 @@
+#lang racket
+(require racket/gui/base)
+(require racket/draw)
+
+(require "./events.rkt")
+
+
+;;;;; GUI
+;
+; Naming convention for GUI elements:
+;              m-wnd-"type"-"context"
+
+
+
+;; Main window
+(provide m-wnd)
+(define m-wnd (new frame%
+                   [label "SVG-Edit"]
+                   [x 0]
+                   [y 0]
+                   [width 800]
+                   [height 600]))
+
+;; Main window pane
+(provide m-wnd-pane)
+(define m-wnd-pane (new horizontal-pane% [parent m-wnd]))
+
+
+
+;; Tool pane
+(provide m-wnd-tool-pane)
+(define m-wnd-tool-pane (new vertical-pane%
+                             [parent m-wnd-pane]
+                             [min-width 50]	 
+                             [min-height 600]	 
+                             [stretchable-width #f]	 
+                             [stretchable-height #f]))
+
+;;Tool pane buttons
+(new message% [parent m-wnd-tool-pane]
+     [label "Toolbar"])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+(new button% [parent m-wnd-tool-pane]
+     [label "--?--"]
+     [min-width 50]
+     [min-height 40]
+     [callback tool-call0])
+
+;;;Short Info Pane (nothing major, just program instructions? feedback?)
+;;(new message% [parent m-wnd]
+;;     [label "Information Panel\n"])
+
+; Button pane
+(provide button-pane)
+(define button-pane (new horizontal-pane% [parent m-wnd]
+                         [alignment (list 'center 'center)]))
+
+(new button% [parent button-pane]
+     [label "?"]
+     [callback button-call0])
+(new button% [parent button-pane]
+     [label "Clear"]
+     [callback button-call0])
+(new button% [parent button-pane]
+     [label "Save"]
+     [callback button-call0])
+(new button% [parent button-pane]
+     [label "Test Color"]
+     [callback button-call0])
+(new button% [parent button-pane]
+     [label "?"]
+     [callback button-call0])
+
+
+(send m-wnd show #t)
---
+++ main.rkt
@@ -0,0 +1,72 @@
+#lang racket
+(require racket/gui/base)
+(require racket/draw)
+
+(require "./gui.rkt")
+
+(define mouse-d-pos (list 0 0))
+
+(define tstE (λ(obj e)(begin (send canv-bitmap-dc clear)
+                             (set! obj-list (list '(0 0 0 0)))
+                             (send m-wnd-canv refresh-now))))
+
+
+(define tst (λ(obj e) 'a))
+(define p-callback (λ (canvas dc) (send dc draw-bitmap canv-bitmap 0 0)))
+
+(define obj-list (list '(0 0 0 0)))
+(define (del-obj obj olist) (filter (λ(x)(not(equal? x obj))) olist))
+(define draw-object (λ(x y x2 y2)(send canv-bitmap-dc draw-line
+                                        x
+                                        y
+                                        x2
+                                        y2)))
+(define (draw-object-all o-list)
+  (map (λ(x)(apply draw-object x)) o-list))
+
+
+;Canvas% class override for event handling
+(define s-canvas%
+  (class canvas% 
+    (define/override (on-event event)
+      (begin
+        (send canv-bitmap-dc clear)
+        (draw-object-all obj-list)
+        (cond
+          ((send event button-down?)
+           (set! mouse-d-pos (list (send event get-x)
+                                         (send event get-y))))
+          ((send event button-up?)
+           (set! obj-list (append obj-list
+                                 (list (append mouse-d-pos
+                                               (list (send event get-x)
+                                                     (send event get-y)))))))
+          ((send event dragging?)
+           ;(begin
+             (send canv-bitmap-dc draw-line
+                   (car mouse-d-pos)
+                   (cadr mouse-d-pos)
+                   (send event get-x)
+                   (send event get-y))
+             ;)
+           ))
+        (send m-wnd-canv refresh-now)))
+    (define/override (on-char event)
+      'a)
+    (super-new)))
+
+
+;;;Main Window Canvas, and canvas dc
+;Canvas is placed in a main window pane defined in GUI.rkt
+(define m-wnd-canv (new s-canvas%
+                        [parent m-wnd-pane]
+                        [paint-callback p-callback]))
+
+(define m-wnd-canv-dc (send m-wnd-canv get-dc))
+
+;; Bitmap for drawing, and bitmap-dc
+(provide canv-bitmap)
+(provide canv-bitmap-dc)
+
+(define canv-bitmap (make-object bitmap% 1 1))
+(define canv-bitmap-dc (new bitmap-dc% [bitmap canv-bitmap]))
