4053ae6ea548d347b3796ef771b31728383b48c1
DKheifets <dkheifets@gmail.com>
2016-04-16 18:04:00 -0400

    Delete events.rkt

--- events.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket
-(provide tool-call0)
-(provide button-call0)
-
-(define tool-call0 (λ (object e) (display "\nYou clicked a toolbar button!\n")))
-(define button-call0 (λ (object e) (display "\nYou clicked a button!\n")))
