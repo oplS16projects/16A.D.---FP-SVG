836d38df05f5670ba4de8ebc3df7b4f08e827691
tetra-D <dmitri.kheifets@gmail.com>
2016-04-25 02:12:05 -0400

    Adding color chooser

--- gui.rkt
+++ gui.rkt
@@ -43,7 +43,7 @@
   ; GUI Callbacks
   ; --------------
   
-  ; Canvas callback
+  ; Main canvas callback
   (define p-callback
     (λ (canvas dc)
       (send dc
@@ -62,7 +62,7 @@
              (cond ((eq? current-util 'save)
                     [(svg 'save) (put-file)])))))
 
-  
+
   ; Selected tool
   (define current-tool '())
   ; Common tool-box buttons callback
@@ -77,13 +77,117 @@
 
   ;; Main window frame
   (define m-wnd (new frame%
-                     [label "SVG-Edit"]
+                     [label "SkraM-SVG"]
                      [x 0]
                      [y 0]
                      [min-width 800]
                      [min-height 600]
                      [stretchable-width #f]	 
                      [stretchable-height #f]))
+  
+  ; =========================================================
+  ;; Properties window
+
+  ;; Make current Pen/Brush color, associated procs
+  (define c-pen '())
+  (define c-brush '())
+  
+  (define (mk-color clr [alpha 1.0])
+    (make-object
+        color%
+      (car clr)
+      (cadr clr)
+      (caddr clr)
+      alpha))
+
+  (define (mk-pen clr)
+    (set! c-pen (new pen%
+                     [color clr]
+                     [width 5]
+                     [style 'solid])))
+  (define (mk-brush clr)
+    (set! c-brush (new brush% [color clr])))
+
+  (mk-pen (mk-color '(0 0 0)))
+  (mk-brush (mk-color '(0 0 0) 0.0))
+
+
+  ; Color select canvas callback
+  (define c-callback
+    (λ (canvas dc)
+      (send dc
+            draw-bitmap
+            bmp-c
+            0 0)))
+
+  ; Color sliders common callback
+  (define color-callback
+    (λ (obj event)
+      (begin
+        (define colors
+          (map (λ (x)(send (car x) get-value))
+               color-sliders))
+        (mk-pen (mk-color colors))
+        
+        (send bmp-c-dc set-pen c-pen)
+        (send bmp-c-dc set-brush c-brush)
+        (send bmp-c-dc draw-ellipse
+                   30
+                   50
+                   140
+                   50)
+        (send p-wnd-canvas refresh-now))))
+  
+  ; Properties window frame
+  (define p-wnd (new frame%
+                     [label "Color"]
+                     [x 800]
+                     [y 0]
+                     [min-width 200]
+                     [min-height 300]
+                     [style (list 'no-system-menu)]
+                     [parent m-wnd]
+                     [stretchable-width #f]	 
+                     [stretchable-height #f]))
+
+  (define p-wnd-slider_pane (new vertical-pane% [parent p-wnd]
+                                 [alignment (list 'center 'center)]))
+
+  (define p-wnd-canvas (new canvas%
+                            [parent p-wnd-slider_pane]
+                            ;[min-width 50]	 
+                            ;[min-height min-height]
+                            [paint-callback c-callback]))
+
+  
+  ;; Bitmap for color selection, and bitmap-dc
+  (define bmp-c (make-object bitmap% 200 300))
+  (define bmp-c-dc (new bitmap-dc%
+                      [bitmap bmp-c]))
+
+   
+  (define (mk-sliders gui_parent slider_list callproc)
+    (map (λ(x) (list (new slider%
+                          [label (car x)]	 
+                          [min-value (car (cadr x))]	 
+                          [max-value (cadr (cadr x))]	 
+                          [parent gui_parent]	 
+                          [callback callproc]	 
+                          [init-value (caddr x)])
+                     (cadddr x)))
+         slider_list))
+  
+  (define sliders-lst (list '("R" (0 255) 120 'red-s)
+                            '("G" (0 255) 120 'green-s)
+                            '("B" (0 255) 120 'blue-s)))
+
+  (define color-sliders (mk-sliders p-wnd-slider_pane
+                                    sliders-lst
+                                    color-callback))
+    
+  ; ===========================================================
+  
+
 
   ;; Main window pane
   ;(provide m-wnd_pane)
@@ -170,7 +274,9 @@
 ; ---------------------------
 
   ;; dispaly GUI
-  (define (show-gui) (send m-wnd show #t))
+  (define (show-gui)
+    (send m-wnd show #t)
+    (send p-wnd show #t))
 
   ;; Resize bitmap to fit canvas
   (define (bmp-resize)
@@ -180,11 +286,13 @@
     (send bmp-dc
           set-bitmap bmp))
 
-  ;; Set drawing canvas
+  ;; Set drawing canvas (also, make init run of
+  ;; color-callback
   (define (set-canvas canvas)
     (set! m-wnd-canvas (new canvas
                             [parent m-wnd_pane]
-                            [paint-callback p-callback])))
+                            [paint-callback p-callback]))
+    (color-callback #f #f))
 
   ;; Set drawn shapes list to operate on
   (define svg '())
@@ -222,6 +330,8 @@
           ((eq? msg 'get-current-util) current-util)
           ((eq? msg 'get-util-btns) util-buttons)
           ((eq? msg 'get-tool-btns) tool-box-buttons)
+          ((eq? msg 'get-pen) c-pen)
+          ((eq? msg 'get-brush) c-brush)
           (else (display "No such thing in here"))))
   dispatch)
           
